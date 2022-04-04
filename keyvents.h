db782b7b897c64028ab926fcc21f5a3bd47ea715
Nicolas <73864341+FISHARMNIC@users.noreply.github.com>
2022-04-03 22:08:19 -0400

    Add files via upload

---
+++ keyvents.h
@@ -0,0 +1,99 @@
+#ifndef STDIO_H
+#include <stdio.h>
+#endif
+
+#ifndef STDARG_H
+#include <stdarg.h>
+#endif
+
+#ifndef PTHREAD_H
+#include <pthread.h>
+#endif
+
+#ifndef TERMIOS_H
+#include <termios.h>
+#endif
+
+static void *__key_events__[255];
+static char __keys_used__[255];
+static char __use_multithread__[255];
+static int __use_key__ = 0;
+
+static char __use_global_event__ = 0;
+static void *__global_trigger__;
+static int __num_events__;
+
+// static char __use_multithread__ = 1;
+
+static struct termios __oldt__, __newt__;
+typedef void void_void_func_ptr_t(void);
+typedef void void_char_func_ptr_t(char);
+
+#define TERM_MT()        \
+    {                       \
+        pthread_exit(NULL); \
+    }
+
+#define USE_MT 0
+#define NO_MT 1
+
+// activate the special interface
+#define KEY_INIT_ENTER() ({                      \
+    tcgetattr(STDIN_FILENO, &__oldt__);          \
+    __newt__ = __oldt__;                         \
+    __newt__.c_lflag &= ~(ICANON);               \
+    tcsetattr(STDIN_FILENO, TCSANOW, &__newt__); \
+})
+
+// activate the key press events
+#define KEY_INIT_EXIT() ({                                         \
+    pthread_t __thread__;                                          \
+    pthread_create(&__thread__, NULL, __kb_event_handler__, NULL); \
+    tcsetattr(STDIN_FILENO, TCSANOW, &__oldt__);                   \
+})
+
+void use_key(char key, void *handler, ...)
+{
+    va_list args;
+    va_start(args, handler);
+    __use_multithread__[__use_key__] = va_arg(args, int);
+    __keys_used__[__use_key__++] = key;
+    __key_events__[key] = handler;
+    __num_events__++;
+}
+
+void use_global(void *handler)
+{
+    __use_global_event__ = 1;
+    __global_trigger__ = handler;
+}
+
+void *__kb_event_handler__(void *arg)
+{
+    char gotten;
+    while (1)
+    {
+        // wait until something got pressed
+        while ((gotten = getchar()) == 0)
+            ;
+
+        //"unprint" the printed character
+        printf("\33[2K\33[A\r");
+
+        // if you are using a global triger
+        if (__global_trigger__)
+            ((void_char_func_ptr_t *)(__global_trigger__))(gotten);
+
+        // for each bound event
+        pthread_t threads[__num_events__];
+        for (int i = 0; i < __num_events__; i++)
+            // if I bound this key
+            if (gotten == __keys_used__[i])
+            {
+                if (__use_multithread__[i] != NO_MT)
+                    pthread_create(&threads[i], NULL, (__key_events__[__keys_used__[i]]), NULL);
+                else
+                    ((void_void_func_ptr_t *)(__key_events__[__keys_used__[i]]))();
+            }
+    }
+}
--- main.c
+++ main.c
@@ -1,19 +1,24 @@
-#include "cvents.h"
+#include "keyvents.h"
 #include <unistd.h>
 
 void key_a_event()
 {
-    printf("\n -> apples\n");
+    printf("private handler apples\n");
+    TERM_MT();
 }
 
 void key_b_event()
 {
-    printf("\n -> bannanas\n");
+    printf("private handler bannanas\n");
 }
 
+void key_c_event()
+{
+    printf("private handler chicken\n");
+}
 void global_event(char c)
 {
-    printf("\n YOU CLICKED: %c\n", c);
+    printf("\nglobal handler: %c\n", c);
 }
 
 void initiate_keys()
@@ -21,8 +26,9 @@ void initiate_keys()
     KEY_INIT_ENTER();
 
     /* Here are your events */
-    use_key('a', key_a_event);
-    use_key('b', key_b_event);
+    use_key('a', key_a_event, USE_MT);
+    use_key('b', key_b_event, NO_MT);
+    use_key('c', key_c_event);
     //activate a global event
     use_global(global_event);
     /* ------------ */
@@ -36,9 +42,9 @@ int main()
 
     // to show how it is multi-threaded
     for(int i = 0;;i++) {
-        printf("%i\n",i);
+        printf("--TIMESTAMP: %i\n",i);
         sleep(1);
     }
-    
+
     return 0;
 }
